e503432f297fd261f2d5a58543730d03be45aab0
Nicolas Emanuel <niculas20003@outlook.com>
2026-06-16 10:44:54 -0300

    feat: adiciona componente LiveCounter com template, estilos e testes

---
+++ frontend/src/app/features/live-counter/live-counter.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LiveCounter } from './live-counter';
+
+describe('LiveCounter', () => {
+  let component: LiveCounter;
+  let fixture: ComponentFixture<LiveCounter>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LiveCounter],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LiveCounter);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/features/live-counter/live-counter.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-counter',
+  imports: [],
+  templateUrl: './live-counter.html',
+  styleUrl: './live-counter.scss',
+})
+export class LiveCounter {}
