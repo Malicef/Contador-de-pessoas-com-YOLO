e24e6a35d16e761a8a60d872fcdcdc83f82ecf69
Nicolas Emanuel <niculas20003@outlook.com>
2026-06-16 10:48:24 -0300

    feat: adiciona rotas para os componentes Home, LiveCounter e UploadCounter

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home')
+        .then(m => m.Home)
+  },
+  {
+    path: 'live',
+    loadComponent: () =>
+      import('./features/live-counter/live-counter')
+        .then(m => m.LiveCounter)
+  },
+  {
+    path: 'upload',
+    loadComponent: () =>
+      import('./features/upload-counter/upload-counter')
+        .then(m => m.UploadCounter)
+  }
+];
